946b54cd5a84d18a530a9e5b880aabf8e75e4009
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-08-22 20:46:05 +0500

    1st

---
+++ babble/src/main/java/edu/westga/cs/babble/Babble.java
@@ -0,0 +1,24 @@
+package edu.westga.cs.babble;
+
+import edu.westga.cs.babble.views.GUI;
+
+/**
+ * Main class for the Babble project
+ * 
+ * @author lewisb
+ * @version cs6241
+ */
+public class Babble {
+    /**
+     * Start point for the application.
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+    	//
+    	GUI gui = new GUI();
+		gui.run();
+    	//
+    }
+
+}
